cbe69169abc720f07f3f400c71b0ae94d07b25b6
ReyaadElkomy <reyaadelkomy@hotmail.com>
2026-04-19 15:25:16 +0200

    Angular basics, create navbar using tailwind css and working with http

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,12 +1,31 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './layout/header/header.component';
+import { HttpClient } from '@angular/common/http';
+import { Product } from './shared/models/product';
+import { Pagination } from './shared/models/pagination';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,HeaderComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
+  baseUrl = 'https://localhost:5051/api/';
+  private http = inject(HttpClient);
   title = 'Skinet';
+  products: Product[] = [];
+
+  ngOnInit(): void {
+    this.http.get<Pagination<Product>>(this.baseUrl + 'products').subscribe({
+      next: (response) => 
+        { 
+          this.products = response.data; 
+          console.log(this.products);
+        }, 
+      error: (error) => { console.log(error) }, 
+      complete: () => { console.log('complete!');}
+    })
+  }
 }
